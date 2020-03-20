823cf22b33d4150bbed804e01bcc547bfe421b27
Aditya Padwal <adityapadwal@Adityas-MacBook-Pro.local>
2020-03-20 22:39:11 +0530

    Queried result through express

--- fabcar/javascript/server.js
+++ fabcar/javascript/server.js
@@ -2,6 +2,59 @@ const express = require('express')
 const app = express()
 const port = 3000
 const HOST = 'localhost'
-app.get('/', (req, res) => res.send('Hello World!'))
+'use strict';
+
+const { Gateway, Wallets } = require('fabric-network');
+const path = require('path');
+const fs = require('fs');
+app.get('/api/query', function(req, res){
+    async function main() {
+        try {
+            // load the network configuration
+            const ccpPath = path.resolve(__dirname, '..', '..', 'first-network', 'connection-org1.json');
+            const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+    
+            // Create a new file system based wallet for managing identities.
+            const walletPath = path.join(process.cwd(), 'wallet');
+            const wallet = await Wallets.newFileSystemWallet(walletPath);
+            console.log(`Wallet path: ${walletPath}`);
+    
+            // Check to see if we've already enrolled the user.
+            const identity = await wallet.get('user1');
+            if (!identity) {
+                console.log('An identity for the user "user1" does not exist in the wallet');
+                console.log('Run the registerUser.js application before retrying');
+                return;
+            }
+    
+            // Create a new gateway for connecting to our peer node.
+            const gateway = new Gateway();
+            await gateway.connect(ccp, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
+    
+            // Get the network (channel) our contract is deployed to.
+            const network = await gateway.getNetwork('mychannel');
+    
+            // Get the contract from the network.
+            const contract = network.getContract('fabcar');
+    
+            // Evaluate the specified transaction.
+            // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
+            // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
+            const result = await contract.evaluateTransaction('queryAllCars');
+            console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+            res.status(200).json({response: `${result.toString()}`})
+    
+        } catch (error) {
+            console.error(`Failed to evaluate transaction: ${error}`);
+            res.status(500).json({error: `${error}`})
+            process.exit(1);
+        }
+    }
+    
+    main();
+    
+}) 
+    
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
