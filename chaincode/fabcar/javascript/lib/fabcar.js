6f0755b764bfa8854197e0e9b8bf34c91674a698
Aditya Padwal <adityapadwal@Adityas-MacBook-Pro.local>
2020-03-24 06:17:58 +0530

    Patient Register, Login, Query APIs

--- chaincode/fabcar/javascript/lib/fabcar.js
+++ chaincode/fabcar/javascript/lib/fabcar.js
@@ -3,111 +3,67 @@
  */
 
 'use strict';
-
 const { Contract } = require('fabric-contract-api');
 
 class FabCar extends Contract {
 
     async initLedger(ctx) {
         console.info('============= START : Initialize Ledger ===========');
-        const cars = [
+        const patients = [
             {
-                color: 'blue',
-                make: 'Toyota',
-                model: 'Prius',
-                owner: 'Tomoko',
+                fullName: 'Test Name',
+                email: 'testemail@email.com',
+                gender: 'Male',
+                dob: '12/12/1997',
+                height: '172cm',
+                weight: '70kg',
+                auth : {
+                    password: 'password'
+                }
             },
-            {
-                color: 'red',
-                make: 'Ford',
-                model: 'Mustang',
-                owner: 'Brad',
-            },
-            {
-                color: 'green',
-                make: 'Hyundai',
-                model: 'Tucson',
-                owner: 'Jin Soo',
-            },
-            {
-                color: 'yellow',
-                make: 'Volkswagen',
-                model: 'Passat',
-                owner: 'Max',
-            },
-            {
-                color: 'black',
-                make: 'Tesla',
-                model: 'S',
-                owner: 'Adriana',
-            },
-            {
-                color: 'purple',
-                make: 'Peugeot',
-                model: '205',
-                owner: 'Michel',
-            },
-            {
-                color: 'white',
-                make: 'Chery',
-                model: 'S22L',
-                owner: 'Aarav',
-            },
-            {
-                color: 'violet',
-                make: 'Fiat',
-                model: 'Punto',
-                owner: 'Pari',
-            },
-            {
-                color: 'indigo',
-                make: 'Tata',
-                model: 'Nano',
-                owner: 'Valeria',
-            },
-            {
-                color: 'brown',
-                make: 'Holden',
-                model: 'Barina',
-                owner: 'Shotaro',
-            },
+         
         ];
 
-        for (let i = 0; i < cars.length; i++) {
-            cars[i].docType = 'car';
-            await ctx.stub.putState('CAR' + i, Buffer.from(JSON.stringify(cars[i])));
-            console.info('Added <--> ', cars[i]);
+        for (let i = 0; i < patients.length; i++) {
+            patients[i].docType = 'patient';
+            await ctx.stub.putState(patients[i].email, Buffer.from(JSON.stringify(patients[i])));
+            console.info('Added <--> ', patients[i]);
         }
         console.info('============= END : Initialize Ledger ===========');
     }
 
-    async queryCar(ctx, carNumber) {
-        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
-        if (!carAsBytes || carAsBytes.length === 0) {
-            throw new Error(`${carNumber} does not exist`);
+    async queryPatient(ctx, patientId) {
+        const patientAsBytes = await ctx.stub.getState(patientId); // get the car from chaincode state
+        if (!patientAsBytes || patientAsBytes.length === 0) {
+            throw new Error(`${patientId} does not exist`);
         }
-        console.log(carAsBytes.toString());
-        return carAsBytes.toString();
+        console.log(patientAsBytes.toString());
+        return patientAsBytes.toString();
     }
 
-    async createCar(ctx, carNumber, make, model, color, owner) {
-        console.info('============= START : Create Car ===========');
-
-        const car = {
-            color,
-            docType: 'car',
-            make,
-            model,
-            owner,
+    async createPatient(ctx, patientInfo) {
+        console.info('============= START : Create Patient ===========');
+        let patientInfoObj = JSON.parse(patientInfo);
+        const patient = {
+           fullName: patientInfoObj.fullName,
+           email: patientInfoObj.email,
+           gender: patientInfoObj.gender,
+           dob: patientInfoObj.dob,
+           height: patientInfoObj.height,
+           weight: patientInfoObj.weight,
+           auth: {
+               password: patientInfoObj.auth.password
+           },
+           docType: 'patient'
         };
 
-        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
-        console.info('============= END : Create Car ===========');
+        await ctx.stub.putState(patient.email, Buffer.from(JSON.stringify(patient)));
+        console.info('============= END : Create Patient ===========');
     }
 
-    async queryAllCars(ctx) {
-        const startKey = 'CAR0';
-        const endKey = 'CAR999';
+    async queryAllPatients(ctx) {
+        const startKey = 'a';
+        const endKey = 'z';
         const allResults = [];
         for await (const {key, value} of ctx.stub.getStateByRange(startKey, endKey)) {
             const strValue = Buffer.from(value).toString('utf8');
@@ -118,25 +74,26 @@ class FabCar extends Contract {
                 console.log(err);
                 record = strValue;
             }
-            allResults.push({ Key: key, Record: record });
+            delete record.auth;
+            allResults.push(record);
         }
         console.info(allResults);
         return JSON.stringify(allResults);
     }
 
-    async changeCarOwner(ctx, carNumber, newOwner) {
-        console.info('============= START : changeCarOwner ===========');
+    // async changeCarOwner(ctx, patientId, newOwner) {
+    //     console.info('============= START : changeCarOwner ===========');
 
-        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
-        if (!carAsBytes || carAsBytes.length === 0) {
-            throw new Error(`${carNumber} does not exist`);
-        }
-        const car = JSON.parse(carAsBytes.toString());
-        car.owner = newOwner;
+    //     const patientAsBytes = await ctx.stub.getState(patientId); // get the car from chaincode state
+    //     if (!patientAsBytes || patientAsBytes.length === 0) {
+    //         throw new Error(`${patientId} does not exist`);
+    //     }
+    //     const car = JSON.parse(patientAsBytes.toString());
+    //     car.owner = newOwner;
 
-        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
-        console.info('============= END : changeCarOwner ===========');
-    }
+    //     await ctx.stub.putState(patientId, Buffer.from(JSON.stringify(car)));
+    //     console.info('============= END : changeCarOwner ===========');
+    // }
 
 }
 
--- fabcar/javascript/server.js
+++ fabcar/javascript/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const port = 3000
 const HOST = 'localhost'
 'use strict';
@@ -7,7 +9,12 @@ const HOST = 'localhost'
 const { Gateway, Wallets } = require('fabric-network');
 const path = require('path');
 const fs = require('fs');
-app.get('/api/query', function(req, res){
+const FabricCAServices = require('fabric-ca-client');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.get('/api/getAllPatients', function(req, res){
+    var email = req.query.email;
     async function main() {
         try {
             // load the network configuration
@@ -20,16 +27,16 @@ app.get('/api/query', function(req, res){
             console.log(`Wallet path: ${walletPath}`);
     
             // Check to see if we've already enrolled the user.
-            const identity = await wallet.get('user1');
+            const identity = await wallet.get(email);
             if (!identity) {
-                console.log('An identity for the user "user1" does not exist in the wallet');
+                console.log('An identity for the user '+email+' does not exist in the wallet');
                 console.log('Run the registerUser.js application before retrying');
                 return;
             }
     
             // Create a new gateway for connecting to our peer node.
             const gateway = new Gateway();
-            await gateway.connect(ccp, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
+            await gateway.connect(ccp, { wallet, identity: email, discovery: { enabled: true, asLocalhost: true } });
     
             // Get the network (channel) our contract is deployed to.
             const network = await gateway.getNetwork('mychannel');
@@ -40,9 +47,10 @@ app.get('/api/query', function(req, res){
             // Evaluate the specified transaction.
             // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
             // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
-            const result = await contract.evaluateTransaction('queryAllCars');
+
+            const result = await contract.evaluateTransaction('queryAllPatients');
             console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
-            res.status(200).json({response: `${result.toString()}`})
+            res.status(200).send({response: JSON.parse(`${result}`)})
     
         } catch (error) {
             console.error(`Failed to evaluate transaction: ${error}`);
@@ -54,7 +62,226 @@ app.get('/api/query', function(req, res){
     main();
     
 }) 
+
+
+
+app.get('/api/getPatient', function(req, res){
+    var email = req.query.email;
+    async function main() {
+        try {
+            // load the network configuration
+            const ccpPath = path.resolve(__dirname, '..', '..', 'first-network', 'connection-org1.json');
+            const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
     
+            // Create a new file system based wallet for managing identities.
+            const walletPath = path.join(process.cwd(), 'wallet');
+            const wallet = await Wallets.newFileSystemWallet(walletPath);
+            console.log(`Wallet path: ${walletPath}`);
+    
+            // Check to see if we've already enrolled the user.
+            const identity = await wallet.get(email);
+            if (!identity) {
+                console.log('An identity for the user '+email+' does not exist in the wallet');
+                console.log('Run the registerUser.js application before retrying');
+                return;
+            }
+    
+            // Create a new gateway for connecting to our peer node.
+            const gateway = new Gateway();
+            await gateway.connect(ccp, { wallet, identity: email, discovery: { enabled: true, asLocalhost: true } });
+    
+            // Get the network (channel) our contract is deployed to.
+            const network = await gateway.getNetwork('mychannel');
+    
+            // Get the contract from the network.
+            const contract = network.getContract('fabcar');
+    
+            // Evaluate the specified transaction.
+            // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
+            // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
+
+            const result = await contract.evaluateTransaction('queryPatient',email);
+            console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+            res.status(200).send({response: JSON.parse(`${result}`)})
+    
+        } catch (error) {
+            console.error(`Failed to evaluate transaction: ${error}`);
+            res.status(500).json({error: `${error}`})
+            process.exit(1);
+        }
+    }
+    
+    main();
+    
+}) 
 
 
+
+app.post('/api/register', function(req,res){
+    var email = req.body.email;
+    var password = req.body.password;
+    var fullName = req.body.fullName;
+    var confirmPassword = req.body.confirmPassword;
+    var height = req.body.height;
+    var weight = req.body.weight;
+    var gender = req.body.gender;
+    var dob = req.body.dob;
+    console.log(password)
+    async function main() {
+        try {
+            // load the network configuration
+            const ccpPath = path.resolve(__dirname, '..', '..', 'first-network', 'connection-org1.json');
+            const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+    
+            // Create a new CA client for interacting with the CA.
+            const caURL = ccp.certificateAuthorities['ca.org1.example.com'].url;
+            const ca = new FabricCAServices(caURL);
+    
+            // Create a new file system based wallet for managing identities.
+            const walletPath = path.join(process.cwd(), 'wallet');
+            const wallet = await Wallets.newFileSystemWallet(walletPath);
+            console.log(`Wallet path: ${walletPath}`);
+    
+            // Check to see if we've already enrolled the user.
+            const userIdentity = await wallet.get(email);
+            if (userIdentity) {
+                console.log('An identity for the user '+email+' already exists in the wallet');
+                return;
+            }
+    
+            // Check to see if we've already enrolled the admin user.
+            const adminIdentity = await wallet.get('admin');
+            if (!adminIdentity) {
+                console.log('An identity for the admin user "admin" does not exist in the wallet');
+                console.log('Run the enrollAdmin.js application before retrying');
+                return;
+            }
+    
+            // build a user object for authenticating with the CA
+            const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
+            const adminUser = await provider.getUserContext(adminIdentity, 'admin');
+    
+            // Register the user, enroll the user, and import the new identity into the wallet.
+            const secret = await ca.register({
+                affiliation: 'org1.department1',
+                enrollmentID: email,
+                role: 'client'
+            }, adminUser);
+            const enrollment = await ca.enroll({
+                enrollmentID: email,
+                enrollmentSecret: secret
+            });
+            const x509Identity = {
+                credentials: {
+                    certificate: enrollment.certificate,
+                    privateKey: enrollment.key.toBytes(),
+                },
+                mspId: 'Org1MSP',
+                type: 'X.509',
+            };
+            await wallet.put(email, x509Identity);
+            console.log('Successfully registered and enrolled admin user '+email+' and imported it into the wallet');
+
+            // Create a new gateway for connecting to our peer node.
+            const gateway = new Gateway();
+            await gateway.connect(ccp, { wallet, identity: email, discovery: { enabled: true, asLocalhost: true } });
+
+            // Get the network (channel) our contract is deployed to.
+            const network = await gateway.getNetwork('mychannel');
+
+            // Get the contract from the network.
+            const contract = network.getContract('fabcar');
+
+            // Submit the specified transaction.
+            // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
+            // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
+            let patientInfo = {
+                fullName: fullName,
+                email: email,
+                gender: gender,
+                dob: dob,
+                height: height,
+                weight: weight,
+                auth: {
+                    password: password
+                },
+                docType: 'patient'
+            }
+            await contract.submitTransaction('createPatient',JSON.stringify(patientInfo));
+            console.log('Transaction has been submitted');
+
+            // Disconnect from the gateway.
+            await gateway.disconnect();
+
+            res.status(200).json({token: 'abcdefg'});
+            
+    
+        } catch (error) {
+            console.error(`Failed to register user`+email+`: ${error}`);
+            res.status(500).json({error: `${error}`})
+            process.exit(1);
+        }
+    }
+    
+    main();
+    
+})
+
+app.post('/api/login', function(req,res){
+    let email = req.body.email;
+    let password = req.body.password;
+    async function main() {
+        try {
+            // load the network configuration
+            const ccpPath = path.resolve(__dirname, '..', '..', 'first-network', 'connection-org1.json');
+            const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+    
+            // Create a new file system based wallet for managing identities.
+            const walletPath = path.join(process.cwd(), 'wallet');
+            const wallet = await Wallets.newFileSystemWallet(walletPath);
+            console.log(`Wallet path: ${walletPath}`);
+    
+            // Check to see if we've already enrolled the user.
+            const identity = await wallet.get(email);
+            if (!identity) {
+                console.log('An identity for the user '+email+' does not exist in the wallet');
+                console.log('Run the registerUser.js application before retrying');
+                return;
+            }
+    
+            // Create a new gateway for connecting to our peer node.
+            const gateway = new Gateway();
+            await gateway.connect(ccp, { wallet, identity: email, discovery: { enabled: true, asLocalhost: true } });
+    
+            // Get the network (channel) our contract is deployed to.
+            const network = await gateway.getNetwork('mychannel');
+    
+            // Get the contract from the network.
+            const contract = network.getContract('fabcar');
+    
+            // Evaluate the specified transaction.
+            // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
+            // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
+
+            const result = await contract.evaluateTransaction('queryPatient',email);
+            console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+            let parsedResult = JSON.parse(`${result}`);
+            if(password == parsedResult.auth.password) {
+                console.log('Password verified');
+                res.status(200).json({data:{token: parsedResult.email}});
+            }
+            else {
+                console.log('Unable to verify password')
+                res.status(500).json({error: 'Unable to verify password'})
+            }
+    
+        } catch (error) {
+            console.error(`Failed to evaluate transaction: ${error}`);
+            res.status(500).json({error: `${error}`})
+            process.exit(1);
+        }
+    }
+    main();
+
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
