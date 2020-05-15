cc0d20d2f2eee1b43a4fe5235410f478fe3fde85
Shatabdi <shatu.pune@gmail.com>
2020-05-15 03:18:23 -0700

    addUserType , Displaying all doctors/patient/both

--- chaincode/fabcar/javascript/lib/fabcar.js
+++ chaincode/fabcar/javascript/lib/fabcar.js
@@ -4,6 +4,8 @@
 
 'use strict';
 const { Contract } = require('fabric-contract-api');
+const shim = require('fabric-shim');
+const util = require('util');
 
 class FabCar extends Contract {
 
@@ -40,55 +42,7 @@ class FabCar extends Contract {
         console.info('============= END : Initialize Ledger ===========');
     }
 
-    async queryPatient(ctx, patientId) {
-        const patientAsBytes = await ctx.stub.getState(patientId); // get the car from chaincode state
-        if (!patientAsBytes || patientAsBytes.length === 0) {
-            throw new Error(`${patientId} does not exist`);
-        }
-        console.log(patientAsBytes.toString());
-        return patientAsBytes.toString();
-    }
-
-    async createPatient(ctx, patientInfo) {
-        console.info('============= START : Create Patient ===========');
-        let patientInfoObj = JSON.parse(patientInfo);
-        const patient = {
-           fullName: patientInfoObj.fullName,
-           email: patientInfoObj.email,
-           gender: patientInfoObj.gender,
-           dob: patientInfoObj.dob,
-           height: patientInfoObj.height,
-           weight: patientInfoObj.weight,
-           auth: {
-               password: patientInfoObj.auth.password
-           },
-           docType: 'patient'
-        };
-        
-        await ctx.stub.putState(patient.email, Buffer.from(JSON.stringify(patient)));
-        console.info('============= END : Create Patient ===========');
-    }
-
-    async createDoctor(ctx, doctorInfo) {
-        console.info('============= START : Create Doctor ===========');
-        let doctorInfoObj = JSON.parse(doctorInfo);
-        const doctor = {
-            fullName: doctorInfoObj.fullName,
-            email: doctorInfoObj.email,
-            gender: doctorInfoObj.gender,
-            dob: doctorInfoObj.dob,
-            height: doctorInfoObj.height,
-            weight: doctorInfoObj.weight,
-            auth: {
-                password: doctorInfoObj.auth.password
-            },
-            docType: 'doctor'
-        };
-        await ctx.stub.putState(doctor.email, Buffer.from(JSON.stringify(doctor)));
-        console.info('============= END : Create Doctor ===========');
-    }
-
-    async queryAllPatients(ctx) {
+    async queryAllUsers(ctx) {
         const startKey = 'a';
         const endKey = 'z';
         const allResults = [];
@@ -107,6 +61,84 @@ class FabCar extends Contract {
         console.info(allResults);
         return JSON.stringify(allResults);
     }
+
+    async queryAccType(ctx,opt) {
+        const startKey = 'a';
+        const endKey = 'z';
+        const allResults = [];
+        for await (const {key, value} of ctx.stub.getStateByRange(startKey, endKey)) {
+            const strValue = Buffer.from(value).toString('utf8');
+            let record;
+            try {
+                record = JSON.parse(strValue);
+            } catch (err) {
+                console.log(err);
+                record = strValue;
+            }
+            delete record.auth;
+            if(opt=="doctor")
+            {
+                if(record.docType=='doctor')
+                    allResults.push(record);
+            }
+            else{
+                if(record.docType=='patient')
+                    allResults.push(record);
+            }
+        }
+        console.info(allResults);
+        return JSON.stringify(allResults);
+    }
+
+    async queryUser(ctx, userId) {
+        const userAsBytes = await ctx.stub.getState(userId); // get the car from chaincode state
+        if (!userAsBytes || userAsBytes.length === 0) {
+            throw new Error(`${userId} does not exist`);
+        }
+        console.log(userAsBytes.toString());
+        return userAsBytes.toString();
+    }
+
+    async createUser(ctx, userInfo) {
+        console.info('============= START : Create User ===========');
+        let userInfoObj = JSON.parse(userInfo);
+        const user = {
+           fullName: userInfoObj.fullName,
+           email: userInfoObj.email,
+           gender: userInfoObj.gender,
+           dob: userInfoObj.dob,
+           height: userInfoObj.height,
+           weight: userInfoObj.weight,
+           auth: {
+               password: userInfoObj.auth.password
+           }
+        };
+        
+        await ctx.stub.putState(user.email, Buffer.from(JSON.stringify(user)));
+        console.info('============= END : Create User ===========');
+    }
+
+    async addUserDetails(ctx, userInfo) {
+        console.info('============= START : addUserDetails ===========');
+        let userInfoObj = JSON.parse(userInfo);
+        const userAsBytes = await ctx.stub.getState(userInfoObj.email); // get the car from chaincode state
+        if (!userAsBytes || userAsBytes.length === 0) {
+            throw new Error(`${userInfoObj.email} does not exist`);
+        }
+        const user = JSON.parse(userAsBytes.toString());
+        if(userInfoObj.userType == 'doctor')
+        {
+            user.docType = 'doctor';
+        }
+        else if(userInfoObj.userType == 'patient')
+        {
+            user.docType = 'patient';
+        }
+
+        await ctx.stub.putState(userInfoObj.email, Buffer.from(JSON.stringify(user)));
+        console.info('============= END : addUserDetails ===========');
+    }
+
 
     async addDoctorDetails(ctx, doctorInfo) {
         console.info('============= START : addDoctorDetails ===========');
@@ -144,6 +176,62 @@ class FabCar extends Contract {
         await ctx.stub.putState(patientInfoObj.email, Buffer.from(JSON.stringify(patient)));
         console.info('============= END : addPatientDetails ===========');
     }
+
+    async getAllResults(iterator, isHistory) {
+        let allResults = [];
+        while (true) {
+          let res = await iterator.next();
+    
+          if (res.value && res.value.value.toString()) {
+            let jsonRes = {};
+            console.log(res.value.value.toString('utf8'));
+    
+            if (isHistory && isHistory === true) {
+              jsonRes.TxId = res.value.tx_id;
+              jsonRes.Timestamp = res.value.timestamp;
+              jsonRes.IsDelete = res.value.is_delete.toString();
+              try {
+                jsonRes.Value = JSON.parse(res.value.value.toString('utf8'));
+              } catch (err) {
+                console.log(err);
+                jsonRes.Value = res.value.value.toString('utf8');
+              }
+            } else {
+              jsonRes.Key = res.value.key;
+              try {
+                jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
+              } catch (err) {
+                console.log(err);
+                jsonRes.Record = res.value.value.toString('utf8');
+              }
+            }
+            allResults.push(jsonRes);
+          }
+          if (res.done) {
+            console.log('end of data');
+            await iterator.close();
+            console.info(allResults);
+            return allResults;
+          }
+        }
+    }
+
+    async getHistoryForMarble(ctx, args) {
+
+        if (args.length < 1) {
+          throw new Error('Incorrect number of arguments. Expecting 1')
+        }
+        let marbleName = args[0];
+        console.info('- start getHistoryForMarble: %s\n', marbleName);
+    
+        let resultsIterator = await ctx.stub.getHistoryForKey(marbleName);
+        let results = await this.getAllResults(resultsIterator, true);
+    
+        return Buffer.from(JSON.stringify(results));
+    }
+
+
+
 
     
 
